373ce0b82e1df8fe22b36d4b02a7318f96560c7b
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-09-28 12:33:28 -0300

    correção da importação do arquivo

--- Atividade5/js/operacoes.js
+++ Atividade5/js/operacoes.js
@@ -1,19 +1,21 @@
-const entradaNumeroA = prompt('Informe o primeiro número: ', '0');
-const entradaNumeroB = prompt('Informe o segundo número: ', '0');
-const numeros = [entradaNumeroA, entradaNumeroB].map((valor) =>
-    parseFloat(valor)
-);
+function executarOperacoes() {
+    const entradaNumeroA = prompt('Informe o primeiro número: ', '0');
+    const entradaNumeroB = prompt('Informe o segundo número: ', '0');
+    const numeros = [entradaNumeroA, entradaNumeroB].map((valor) =>
+        parseFloat(valor)
+    );
 
-const soma = numeros[0] + numeros[1];
-const subtracao = numeros[0] - numeros[1];
-const produto = numeros[0] * numeros[1];
-const divisao = numeros[0] / numeros[1];
-const restoDivisao = numeros[0] % numeros[1];
+    const soma = numeros[0] + numeros[1];
+    const subtracao = numeros[0] - numeros[1];
+    const produto = numeros[0] * numeros[1];
+    const divisao = numeros[0] / numeros[1];
+    const restoDivisao = numeros[0] % numeros[1];
 
-alert(`
+    alert(`
 ${numeros[0]} + ${numeros[1]} = ${soma}
 ${numeros[0]} - ${numeros[1]} = ${subtracao}
 ${numeros[0]} * ${numeros[1]} = ${produto}
 ${numeros[0]} / ${numeros[1]} = ${divisao}
 Resto da dívisãao "${numeros[0]} / ${numeros[1]}" é ${restoDivisao}
 `);
+}
