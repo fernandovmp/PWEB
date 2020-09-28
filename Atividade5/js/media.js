4923d40461cdea95f3a3059e6a83e8596b67611a
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-09-28 12:06:27 -0300

    Atividade 5

---
+++ Atividade5/js/media.js
@@ -0,0 +1,19 @@
+const nomeAluno = prompt('Informe o nome do aluno: ', '');
+const notas = [];
+for (let i = 0; i < 3; i++) {
+    const entradaNota = prompt(`Informe a nota ${i + 1} do aluno: `, '0');
+    const nota = parseFloat(entradaNota);
+    notas.push(isNaN(nota) ? 0 : nota);
+}
+
+function calcularMediaAritimetica(valores) {
+    const valorInicial = 0;
+    const somatoria = valores.reduce(
+        (somaNotas, nota) => somaNotas + nota,
+        valorInicial
+    );
+    return somatoria / valores.length;
+}
+
+const media = calcularMediaAritimetica(notas);
+alert(`A média final do aluno ${nomeAluno} é ${media}`);
